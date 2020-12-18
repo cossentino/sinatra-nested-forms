a24aec27abe6fa542ff38310b57a59b40eb81b5a
Ian Cossentino <icossentino@gmail.com>
2020-12-18 14:40:58 -0500

    commit tree ba280211be94693cf8c27bba62652f30753b14b3

--- app.rb
+++ app.rb
@@ -3,6 +3,22 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
+    get '/' do
+      erb :'index'
+    end
+
+    get '/new' do
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do
+      @pirate = Pirate.new(params)
+      # @ship_1 = Ship.new(params[:pirate][:ship][0])
+      # @ship_1 = Ship.new(params[:pirate][:ship][1])
+      erb :'pirates/show'
+    end
+
+
     # code other routes/actions here
 
   end
--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,22 @@
+require_relative "./ship"
+
 class Pirate
+
+    attr_accessor :name, :weight, :height, :ship_1, :ship_2
+
+    @@all = []
+
+    def initialize(hash)
+        @name = hash[:pirate][:name]
+        @weight = hash[:pirate][:weight]
+        @height = hash[:pirate][:height]
+        @ship_1 = Ship.new(hash[:pirate][:ships][0])
+        @ship_2 = Ship.new(hash[:pirate][:ships][1])
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
 end
